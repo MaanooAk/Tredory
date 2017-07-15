bbb5bb9940b5a80749caa0a17fbefb84a638cf33
MaanooAk <akritasak@gmail.com>
2017-07-15 12:02:15 +0300

    Engine basic classes holders

---
+++ src/main/java/com/maanoo/tredory/engine/LayeredSprite.java
@@ -0,0 +1,12 @@
+// Tredory Copyright (c) 2014-2017 Tredory author list (see README.md)
+
+package com.maanoo.tredory.engine;
+
+/**
+ * TODO doc
+ *
+ * @author MaanooAk
+ */
+public class LayeredSprite extends Sprite {
+
+}
---
+++ src/main/java/com/maanoo/tredory/engine/Sprite.java
@@ -0,0 +1,13 @@
+// Tredory Copyright (c) 2014-2017 Tredory author list (see README.md)
+
+package com.maanoo.tredory.engine;
+
+
+/**
+ * TODO doc
+ * 
+ * @author MaanooAk
+ */
+public abstract class Sprite {
+
+}
